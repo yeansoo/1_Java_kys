5c8ee5315b2870de06d437d830b736544de70f71
Yeonsoo Kim <raura98@naver.com>
2022-08-05 15:13:18 +0900

    00_javaTest 프로젝트 추가
    클래스 작성법

---
+++ 00_javaTest/src/edu/kh/test/JavaTest.java
@@ -0,0 +1,20 @@
+package edu.kh.test;
+
+//한줄 주석(컴파일러(번역기)가 해석하지 않는 부분)
+
+/*
+범위주석
+*/
+
+//class : 자바 코드가 작동되는 영역
+public class JavaTest {
+ 
+    // main 메서드 : 자바 애플리케이션(프로그램)을 실행하기 위해서 반드시 필요한 구문
+    public static void main(String[] ars) {
+       System.out.println("Hello world");
+       
+       System.out.println("I want to go home");
+       
+       System.out.println(12+100+500);//뀨뀨규끵
+    }
+}
