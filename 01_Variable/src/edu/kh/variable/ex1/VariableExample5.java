5b5b3f9a0fd9a3ba0ad874e56c948225098cdd62
Yeonsoo Kim <raura98@naver.com>
2022-08-05 15:30:13 +0900

    01_variable 프로젝트 추가
    변수, print

---
+++ 01_Variable/src/edu/kh/variable/ex1/VariableExample5.java
@@ -0,0 +1,52 @@
+package edu.kh.variable.ex1;
+
+public class VariableExample5 {
+		
+	public static void main(String[] argc) {
+		
+			//ctrl + alt + 방향키 위 /아래
+		
+			/* 출력 메서드
+			 * 
+			 *  System.out.print("내용");
+			 *  -> () 안의 내용을 출력 ( 줄바꿈 X )
+			 *  
+			 *  System.out.println("내용");
+			 *  -> () 안의 내용을 출력 ( 줄바꿈 O )
+			 *  
+			 *  System.out.printf("내용+패턴", 패턴에 들어갈 값);
+			 *  
+			 *  
+			 */
+		
+		System.out.print("aaaaaaaa");
+		System.out.print("bbbbbbb");
+		System.out.println("lllllll");
+		System.out.println("dddddddddd");
+		
+		String name="김연수";
+		
+		int age=25;
+		
+		char gender='여';
+		
+		double height=169.8;
+		
+		boolean tf=true;
+		
+		System.out.println(name +"님은 "+age+"세 "+gender+"성이고 "+height+"cm입니다 (" + tf + ")" );
+		
+		
+		System.out.printf("%s님은 %d세 %c성이고 %.1fcm입니다 (%b)\n",name,age,gender,height,tf);
+		// \n : 줄바꿈(개행)을 나타내는 탈출(escape) 문자
+		
+		System.out.println("줄 바낌?");
+		
+		
+		
+		
+		
+		
+		
+		}
+}
