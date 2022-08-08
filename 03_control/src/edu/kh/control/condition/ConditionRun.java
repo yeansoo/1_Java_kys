81e83a33e1926c1efaded5f079a5f815b6c1b42f
Yeonsoo Kim <raura98@naver.com>
2022-08-08 17:19:37 +0900

    control + 객체이용법

---
+++ 03_control/src/edu/kh/control/condition/ConditionRun.java
@@ -0,0 +1,19 @@
+package edu.kh.control.condition;
+
+
+
+// 코드를 실행시키는 부분(시키는 사람)
+public class ConditionRun {
+			public static void main(String[] args) {
+				
+				ConditionExample ex=new ConditionExample();
+				
+				// 커서 놓고 -> ctrl + / == 한줄 주석
+//				ex.test1();
+//				ex.test2();
+				
+				
+				ex.ex2();
+				
+			}
+}
